1e02f813704a6dc4a21500a9c9f5a88dfba95dfb
Pedro Serey <serey.pedro@gmail.com>
2024-02-07 11:09:37 -0300

    add: separate output directory in resource monitoring

--- util/run_optimization.sh
+++ util/run_optimization.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 output_directory="$1"
+logs_directory="$2"
 
-./collect-cpu-usage.sh > "$output_directory/cpu_output.csv" &
-./collect-memory-usage.sh > "$output_directory/memory_output.csv" &
+./collect-cpu-usage.sh > "$logs_directory/cpu_output.csv" &
+./collect-memory-usage.sh > "$logs_directory/memory_output.csv" &
 
-nohup ./build/opt.elf /optimizer-files/on_demand_config.csv /optimizer-files/savings_plan_config.csv /optimizer-files/total_demand.csv /optimizer-files/output >/optimizer-logs/output.log 2>/optimizer-logs/error.log
+nohup ./build/opt.elf "$output_directory/on_demand_config.csv" "$output_directory/savings_plan_config.csv" "$output_directory/total_demand.csv" "$output_directory/output" > "$logs_directory/output.log" 2> "$logs_directory/error.log"
 
 ps -ef | grep collect-cpu-usage.sh | grep -v grep | awk '{ print $1 }' | xargs kill -9
 ps -ef | grep collect-memory-usage.sh | grep -v grep | awk '{ print $1 }' | xargs kill -9
